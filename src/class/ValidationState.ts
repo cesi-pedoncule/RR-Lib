bf625e20f3c232eae838c3fae887bb5a889f9dca
Horziox <horziox.dev@gmail.com>
2023-01-24 14:08:18 +0100

    WIP Validation State

---
+++ src/class/ValidationState.ts
@@ -0,0 +1,42 @@
+import User, { UserData } from "./User";
+import { State } from "../builders/ValidStateBuilder";
+import Base from "./Base";
+import Client from "../client/Client";
+
+export interface ValidationStateData {
+    id: string;
+    state: State;
+    updatedAt: string | null;
+
+    moderator: Partial<UserData> | null;
+}
+
+export default class ValidationState extends Base {
+
+    public id: string;
+    public state: State;
+    public updatedAt: Date | null;
+
+    public moderator: User | null;
+
+    constructor(client: Client, data: ValidationStateData) {
+        super(client, data.id, "/validation_states");
+        this.id = data.id;
+        this.state = data.state;
+        this.updatedAt = data.updatedAt ? new Date(data.updatedAt) : null;
+
+        this.moderator = this.getModerator(data.moderator?.id);
+    }
+
+    private getModerator(id?: string | null) {
+        return id ? this.client.users.cache.get(id) ?? null : null;
+    }
+
+    public toJSON() {
+        return {
+            id: this.id,
+            state: this.state,
+        }
+    }
+
+}
