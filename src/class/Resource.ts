24d3b879379f87d16bfa96ced6b72f98a406771d
Horziox <horziox.dev@gmail.com>
2023-02-22 11:02:01 +0100

    add like functions on Resource class

--- src/class/Resource.ts
+++ src/class/Resource.ts
@@ -3,6 +3,7 @@ import { APIResourceData } from "../@types";
 import Base from "./Base";
 import User from "./User";
 import Client from "../client/Client";
+import UserLikeBuilder from "../builders/UserLikeBuilder";
 
 import ResourceAttachmentManager from "../managers/ResourceAttachmentManager";
 import ResourceCategoryManager from "../managers/ResourceCategoryManager";
@@ -45,6 +46,34 @@ export default class Resource extends Base {
 
     private getCreator(id?: string | null) {
         return id ? this.client.users.cache.get(id) ?? null : null;
+    }
+
+    private getYourLike() {
+        if(this.client.auth.me) {
+            const me = this.likes.cache.find(l => l.user?.id === this.client.auth.me?.id);
+            if(me) {
+                return me;
+            }
+        }
+        return null;
+    }
+
+    public hasLike() {
+        return this.getYourLike() ? true : false;
+    }
+
+    public like() {
+        const me = this.client.auth.me;
+        if(me) {
+            this.likes.add(new UserLikeBuilder().setUser(me));
+        }
+    }
+
+    public unlike() {
+        const l = this.getYourLike();
+        if(l) {
+            this.likes.remove(l);
+        }
     }
 
     /** Return data for api request */
